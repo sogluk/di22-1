0857151223ffc0ae324b0169e295b1aa99ba16e4
AlexisMettendorff <alexis.mettendorff@viacesi.fr>
2022-10-24 14:59:59 +0200

    css

--- Groupe3/script.js
+++ Groupe3/script.js
@@ -2,3 +2,9 @@ console.log("coucou");
 var index = document.querySelector('#id1');
 index.addEventListener('mouseover'),function () {
     console.log("Bonjour Romain");};
+var index = document.querySelector('#id2');
+index.addEventListener('mouseover'),function () {
+    console.log("Bonjour Alexis");};
+var index = document.querySelector('#id3');
+index.addEventListener('mouseover'),function () {
+    console.log("Bonjour Killian");};
